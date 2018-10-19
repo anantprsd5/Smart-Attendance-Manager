932aa33f0d1dcb866aad17beac31c7413b7d9dd9
Anant Prasad <anantprsd5@gmail.com>
2018-10-20 01:28:57 +0530

    Added support for landscape for time table layout

--- app/src/main/java/com/example/anant/smartattendancemanager/Activities/DetailActivity.java
+++ app/src/main/java/com/example/anant/smartattendancemanager/Activities/DetailActivity.java
@@ -7,11 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.anant.smartattendancemanager.Adapters.DetailsAdapter;
+import com.example.anant.smartattendancemanager.DatabaseHelper;
 import com.example.anant.smartattendancemanager.Fragments.AttendanceDialogFragment;
 import com.example.anant.smartattendancemanager.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,13 +26,15 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.Map;
 
-public class DetailActivity extends AppCompatActivity implements AttendanceDialogFragment.NoticeDialogListener {
+public class DetailActivity extends AppCompatActivity implements
+        AttendanceDialogFragment.NoticeDialogListener, DatabaseHelper.OnDataFetchedListener {
 
     private Toolbar toolbar;
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLayoutManager;
     private String UID;
     private FirebaseAuth mAuth;
+    private DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +45,29 @@ public class DetailActivity extends AppCompatActivity implements AttendanceDialo
         toolbar.setTitle("Details");
         setSupportActionBar(toolbar);
 
-        //Toolbar is now an action bar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth != null) {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser == null) {
+                Intent intent = new Intent(this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(intent);
+                return;
+            }
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(intent);
+            return;
+        }
+
         mRecyclerView = findViewById(R.id.recycler_view_details);
         mRecyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager = new
+
+                LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         mAuth = FirebaseAuth.getInstance();
@@ -56,32 +76,10 @@ public class DetailActivity extends AppCompatActivity implements AttendanceDialo
 
         // Get a reference to our posts
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference ref = database.getReference("/users/" + UID + "/subjects");
+        ref = database.getReference("/users/" + UID + "/subjects");
 
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
-                if (map != null) {
-                    DetailsAdapter detailsAdapter = new DetailsAdapter(map, new DetailsAdapter.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(String key) {
-                            DialogFragment newFragment = new AttendanceDialogFragment();
-                            ((AttendanceDialogFragment) newFragment).setArguments(ref, key);
-                            Toast.makeText(getApplicationContext(), key, Toast.LENGTH_SHORT).show();
-                            newFragment.show(getSupportFragmentManager(), "attendance");
-
-                        }
-                    });
-                    mRecyclerView.setAdapter(detailsAdapter);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+        DatabaseHelper helper = new DatabaseHelper(UID, this);
+        helper.getSubjects();
     }
 
 
@@ -111,5 +109,22 @@ public class DetailActivity extends AppCompatActivity implements AttendanceDialo
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDataFetched(Map<String, Object> map, boolean isSuccessful) {
+        if (map != null) {
+            DetailsAdapter detailsAdapter = new DetailsAdapter(map, new DetailsAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(String key) {
+                    DialogFragment newFragment = new AttendanceDialogFragment();
+                    ((AttendanceDialogFragment) newFragment).setArguments(ref, key);
+                    Toast.makeText(getApplicationContext(), key, Toast.LENGTH_SHORT).show();
+                    newFragment.show(getSupportFragmentManager(), "attendance");
+
+                }
+            });
+            mRecyclerView.setAdapter(detailsAdapter);
+        }
     }
 }
